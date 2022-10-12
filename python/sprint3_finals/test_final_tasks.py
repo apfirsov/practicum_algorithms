2df83e1f82513c916800221cef221458cb2c5b7f
Artem Firsov <firsov.ip@gmail.com>
2022-10-13 01:24:46 +0300

    task b added, non final

--- python/sprint3_finals/test_final_tasks.py
+++ python/sprint3_finals/test_final_tasks.py
@@ -1,5 +1,7 @@
+import sys
+from io import StringIO
 from a import broken_search
-
+from b import Contestant, main
 
 def test_a() -> None:
     arr = [19, 21, 100, 101, 1, 4, 5, 7, 12]
@@ -8,4 +10,24 @@ def test_a() -> None:
 
 
 def test_b() -> None:
-    pass
+    cases = [
+        ([['alla', '4', '100'], ['gena', '6', '1000'], ['gosha', '2', '90'],
+        ['rita', '2', '90'], ['timofey', '4', '80']],
+        'gena\ntimofey\nalla\ngosha\nrita\n'),
+        ([['alla', '0', '0'], ['gena', '0', '0'], ['gosha', '0', '0'],
+          ['rita', '0', '0'], ['timofey', '0', '0']],
+         'alla\ngena\ngosha\nrita\ntimofey\n'),
+    ]
+
+    for data, exp in cases:
+        contest_list = [Contestant(*params) for params in data]
+        out = StringIO()
+        sys.stdout = out
+        main(contest_list)
+        sys.stdout = sys.__stdout__
+        assert out.getvalue() == exp, 'WA'
+
+
+
+
+
